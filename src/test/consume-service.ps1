edad9ac5010bc55a872295092fa788a4dc8b387a
iformas <iformas@hotmail.com>
2019-03-14 09:37:00 -0300

    agregado script powershell

---
+++ src/test/consume-service.ps1
@@ -0,0 +1,7 @@
+﻿$val = 0
+while($val -le 500)
+     {
+        Write-Host "$(date) - $(Invoke-WebRequest -s -k -TimeoutSec 3 http://cxf-jaxrs-route-hello-word.apps.cnad.io/services/helloservice/sayHello/David)"
+        Start-Sleep -s 1
+       $val++
+     }
